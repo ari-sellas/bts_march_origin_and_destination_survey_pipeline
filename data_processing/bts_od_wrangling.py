dbd9a142a9e15bdec56136109d595bf0acbaf655
ari-sellas <295263497+ari-sellas@users.noreply.github.com>
2026-06-24 12:41:04 -0400

    Ensured there is only one parquet file in the OD zip

--- data_processing/bts_od_wrangling.py
+++ data_processing/bts_od_wrangling.py
@@ -1,10 +1,14 @@
-import zipfile
-
 import requests
 import io
+import zipfile
 
 od_url = "https://ostrapeispubdownloadprod.blob.core.windows.net/ostrapeis-pub-download-prod/ond40/products/db1c_public/DB1C.TICKET.202603.11JUN2026.zip"
 
 response = requests.get(od_url)
 response.raise_for_status()
 zip_buffer = io.BytesIO(response.content)
+
+with zipfile.ZipFile(zip_buffer) as zf:
+    parquet_file = [n for n in zf.namelist() if n.endswith(".parquet")]
+
+    print(parquet_file)
