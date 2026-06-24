b6224291190e5a8608fff5f3b5d7818e4bdadf23
ari-sellas <295263497+ari-sellas@users.noreply.github.com>
2026-06-24 12:54:42 -0400

    Made sure pandas converted to a data frame properly

--- data_processing/bts_od_wrangling.py
+++ data_processing/bts_od_wrangling.py
@@ -1,6 +1,7 @@
 import requests
 import io
 import zipfile
+import pandas as pd
 
 od_url = "https://ostrapeispubdownloadprod.blob.core.windows.net/ostrapeis-pub-download-prod/ond40/products/db1c_public/DB1C.TICKET.202603.11JUN2026.zip"
 
@@ -10,5 +11,7 @@ zip_buffer = io.BytesIO(response.content)
 
 with zipfile.ZipFile(zip_buffer) as zf:
     parquet_file = [n for n in zf.namelist() if n.endswith(".parquet")]
+    parquet_bytes = zf.read(parquet_file[0])
 
-    print(parquet_file)
+df = pd.read_parquet(io.BytesIO(parquet_bytes))
+print(df.head())
